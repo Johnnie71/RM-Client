b2841151e87cc1bdd3aa0adf4f0c917bd807119d
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-31 17:16:19 -0400

    creating sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
+import './App.css';
+
 import { ChannelListContainer, ChannelContainer} from './components';
 
 const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_KEY);
--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
-export default function ChannelListContainer() {
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+
+const sidebar = () => {
+    <div className="channel-list__sidebar">
+        <div className="channel-list_sidebar__icon1">
+            <div className="icon1__inner">
+                
+            </div>
+
+        </div>
+    </div>
+}
+
+const ChannelContainer = () => {
     return (
         <div>
             This is the Channel List Container
         </div>
-    )
-}
+    );
+};
+
+export default ChannelContainer;
