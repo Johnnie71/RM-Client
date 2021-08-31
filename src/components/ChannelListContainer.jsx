2f96b3b8bc1cb7ab5f6171ae58efdfe649c014bc
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-31 17:41:38 -0400

    imported icon

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -3,12 +3,13 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+import UnicornIcon from '../assets/unicorn.png';
 
 const sidebar = () => {
     <div className="channel-list__sidebar">
         <div className="channel-list_sidebar__icon1">
             <div className="icon1__inner">
-                
+                <img src={UnicornIcon} />
             </div>
 
         </div>
