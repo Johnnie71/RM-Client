281a4a2f9cf4bcbef68066f3a41834041072c881
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-31 17:49:04 -0400

    added logout icon

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -5,16 +5,20 @@ import Cookies from 'universal-cookie';
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import UnicornIcon from '../assets/unicorn.png';
 
-const sidebar = () => {
+const Sidebar = () => (
     <div className="channel-list__sidebar">
         <div className="channel-list_sidebar__icon1">
             <div className="icon1__inner">
-                <img src={UnicornIcon} />
+                <img src={UnicornIcon} alt='Unicorn' width='30' />
             </div>
-
+        </div>
+        <div className="channel-list_sidebar__icon2">
+            <div className="icon1__inner">
+                <img src={LogoutIcon} alt='Logout' width='30' />
+            </div>
         </div>
     </div>
-}
+);
 
 const ChannelContainer = () => {
     return (
