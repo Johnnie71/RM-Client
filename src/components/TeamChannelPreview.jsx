7ba5183409b53497fd25ef92e1af8d75c28dda27
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-01 15:19:35 -0400

    DirectMessagesPreview

--- src/components/TeamChannelPreview.jsx
+++ src/components/TeamChannelPreview.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import { Avatar, useChatContext } from 'stream-chat-react';
 
 const TeamChannelPreview = ({ channel, type }) => {
+    const { channel: activeChannel, client} = useChatContext();
+
+    const ChannelPreview = () => (
+        <p className="channel-preview__item">
+            # {channel?.data?.name || channel?.data?.id}
+        </p>
+    );
+
+    const DirectMessPreview = () => {
+        const members = Object.values(channel.state.memmbers).filter(({ user }) => user.id != client.userID);
+    };
+
     return (
         <div>
             
