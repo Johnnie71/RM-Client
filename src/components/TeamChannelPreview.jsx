8186b591f8f8600a438b1775747ce2f8a0409f54
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-01 15:25:39 -0400

    showing members names

--- src/components/TeamChannelPreview.jsx
+++ src/components/TeamChannelPreview.jsx
@@ -12,6 +12,17 @@ const TeamChannelPreview = ({ channel, type }) => {
 
     const DirectMessPreview = () => {
         const members = Object.values(channel.state.memmbers).filter(({ user }) => user.id != client.userID);
+
+        return (
+            <div className='channel-preview__item single'>
+                <Avatar 
+                    image={members[0]?.user?.image}
+                    name={member[0]?.user?.fullName}
+                    size={24}
+                />
+                <p>{members[0]?.user?.fullName}</p>
+            </div>
+        )
     };
 
     return (
