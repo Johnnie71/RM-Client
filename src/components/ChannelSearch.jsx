f0e89b5dbdc2f520d7c70d2be95069db853e0262
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-01 14:15:21 -0400

    set up search

--- src/components/ChannelSearch.jsx
+++ src/components/ChannelSearch.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useChatContext } from 'stream-chat-react';
+
+import { SearchIcon } from '../assets';
 
 const ChannelSearch = () => {
     return (
-        <div>
-            
+        <div className="channel-search__container">
+            <div className="channel-search__input__wrapper">
+                <div className="channel-search__input__icon">
+                    <SearchIcon />
+                </div>
+            </div>
         </div>
     );
 };
