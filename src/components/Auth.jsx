7af6ccbb847ce9d034a0d39c9c4caedbff3cb707
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-02 14:16:40 -0400

    added background image for signup

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
 
+import signinImage from '../assets/signup.jpg';
+
 const Auth = () => {
     return (
         <div>
