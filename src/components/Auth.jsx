361c0aa5590631f48a433ff6e5770c4702e6a558
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-03 14:14:28 -0400

    getting data from the backend and stored in cookies

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 import signupImage from '../assets/signup.webp';
 
+const cookies = new Cookies();
+
 const initialState = {
     fullName: '',
     username: '',
@@ -35,6 +37,17 @@ const Auth = () => {
         const { data: { token, userId, hashedPassword } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName, phoneNumber, avatarURL
         });
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup){
+        cookies.set('phoneNumber', phoneNumber);
+        cookies.set('avatarURL', avatarURL);
+        cookies.set('hashedPassword', hashedPassword);
+        }
     }
 
     return (
