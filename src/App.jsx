63412d090efa2b8edcc867c73be807f587543a12
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-03 14:25:20 -0400

    getting the cookies

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,22 @@ import './App.css';
 
 import { ChannelListContainer, ChannelContainer, Auth} from './components';
 
+const cookies = new Cookies();
+const authToken = cookies.get("token");
+
 const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_KEY);
 
-const authToken = false;
+if(authToken) {
+    client.connectUser({
+        token: cookies.get('token'),
+        username: cookies.get('username'),
+        fullName: cookies.get('fullName'),
+        userId: cookies.get('userId'),
+        phoneNumber: cookies.get('phoneNumber'),
+        avatarURL: cookies.get('avatarURL'),
+        hashedPassword: cookies.get('hashedPassword')
+    });
+};
 
 export default function App() {
 
