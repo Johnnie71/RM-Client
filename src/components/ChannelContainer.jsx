7366322f96eb3699f15d02cb424448305df45974
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-07 19:57:35 -0400

    boiler plate

---
+++ src/components/ChannelContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Channel, useChatContext } from 'stream-chat-react';
+
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './';
+
+const ChannelContainer = () => {
+    
+    return (
+        <div>
+            This is the Channel Container
+        </div>
+    );
+};
+
+export default ChannelContainer;
