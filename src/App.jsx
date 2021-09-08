7307cb94e8326e703eff1b41cd2bdceac3a4a2e6
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-08 15:16:10 -0400

    importing from Stream

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
+// importing from Stream to get a better looking application
+import 'stream-chat-react/dist/css/index';
+
 import './App.css';
 
 import { ChannelListContainer, ChannelContainer, Auth} from './components';
