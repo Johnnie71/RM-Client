578d41cce54caf407638d37d79cb67af156d1ec1
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-08 15:06:35 -0400

    handling empty state when creating new channel

--- src/components/ChannelContainer.jsx
+++ src/components/ChannelContainer.jsx
@@ -25,6 +25,16 @@ const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, 
         );
     };
 
+    // Used when you create a chat and there are no messages yet
+    const EmptyState = () => {
+        return (
+            <div className="channel-empty__container">
+                <p className="channel-empty__first">This is the beginning of your chat history.</p>
+                <p className="channel-empty__second">Send messages, attachments, links emojis and more!</p>
+            </div>
+        )
+    }
+
     return (
         <div>
             This is the Channel Container
