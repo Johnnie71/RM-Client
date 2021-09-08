c79129880b5fbc7480f831c61fa39b78258547a0
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-08 15:52:19 -0400

    fixed bugs

--- src/components/TeamChannelList.jsx
+++ src/components/TeamChannelList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import AddChannel from '../assets/addchannel';
+import AddChannel from '../assets/AddChannel';
 
 const TeamChannelList = ({children, error = false, loading, type}) => {
 
