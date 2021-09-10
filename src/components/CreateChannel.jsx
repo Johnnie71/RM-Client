9bf1d4bcded778c6faa8ce4f64163ee1e644ad04
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-10 19:37:45 -0400

    handling on change for input

--- src/components/CreateChannel.jsx
+++ src/components/CreateChannel.jsx
@@ -6,10 +6,16 @@ import { CloseCreateChannel } from '../assets';
 
 const ChannelNameInput = ({ channelName = '', setChannelName }) => {
 
+    const handleChange = (e) => {
+        e.preventDefault();
+
+        setChannelName(e.target.value);
+    }
+
     return (
         <div className="channel-name-input__wrapper">
             <p>Name</p>
-            <input value={} onChange={} placeholder='channel-name (no spaces)' />
+            <input value={channelName} onChange={handleChange} placeholder='channel-name (no spaces)' />
             <p>Add Members</p>
         </div>
     )
