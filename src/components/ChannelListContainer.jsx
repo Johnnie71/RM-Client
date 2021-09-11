d651ba52926157fc18d0b80994d108a161e4ba8b
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-11 15:12:39 -0400

    adding props to team channel list

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -80,6 +80,10 @@ const ChannelListContainer = ({ isCreating, setIsCreating, setCreateType, setIsE
                         <TeamChannelList 
                             {...listProps}
                             type="messaging"
+                            isCreating={isCreating}
+                            setIsCreating={setIsCreating}
+                            setCreateType={setCreateType}
+                            setIsEditing={setIsEditing}
                         />
                     )}
                     Preview={(previewProps) => (
