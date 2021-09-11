53a1708fbd36747535fc83d526e1c3802fd23533
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-11 15:24:56 -0400

    loading list of users

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,7 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { Children, useEffect, useState } from 'react';
 import { Avatar, useChatChannel } from 'stream-chat-react';
 
 import { InviteIcon } from '../assets';
+
+const ListContainer = ( { children }) => {
+    return (
+        <div className='user-list__container'>
+            <div className='user-list__header'>
+                <p>User</p>
+                <p>Invite</p>
+            </div>
+            {Children}
+        </div>
+    );
+};
 
 
 const UserList = () => {
