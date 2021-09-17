32a6e82fad473e757a4dd873706dab73b925b2c0
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-16 21:21:34 -0400

    fixed onClick

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -12,12 +12,12 @@ const Sidebar = ({ logout }) => (
     <div className="channel-list__sidebar">
         <div className="channel-list__sidebar__icon1">
             <div className="icon1__inner">
-                <img src={ChatIcon} alt='Unicorn' width='30' />
+                <img src={ChatIcon} alt='chat' width='30' />
             </div>
         </div>
         <div className="channel-list__sidebar__icon2">
-            <div className="icon1__inner">
-                <img src={LogoutIcon} alt='Logout' width='30' onClick={logout} />
+            <div className="icon1__inner" onClick={logout}>
+                <img src={LogoutIcon} alt='Logout' width='30'  />
             </div>
         </div>
     </div>
