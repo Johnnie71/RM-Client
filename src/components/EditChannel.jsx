f1cc8e812ccf49138489d060fd79f0f039eb1473
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-16 22:57:41 -0400

    copied channel input in order to be able to edit channel

--- src/components/EditChannel.jsx
+++ src/components/EditChannel.jsx
@@ -1,4 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useChatContext } from 'stream-chat-react';
+
+import { UserList } from './';
+import { CloseCreateChannel } from '../assets';
+
+const ChannelNameInput = ({ channelName = '', setChannelName }) => {
+
+    const handleChange = (e) => {
+        e.preventDefault();
+
+        setChannelName(e.target.value);
+    }
+
+    return (
+        <div className="channel-name-input__wrapper">
+            <p>Name</p>
+            <input value={channelName} onChange={handleChange} placeholder='channel-name' />
+            <p>Add Members</p>
+        </div>
+    )
+}
 
 const EditChannel = () => {
     return (
